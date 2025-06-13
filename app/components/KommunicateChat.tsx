6df72d3ae136212f0f9bdeb64e6ee74056b7e706
MUHAMMADSHAHRUKH459 <freelacing123@gmail.com>
2025-06-14 02:34:57 +0500

    chatbot update

--- app/components/KommunicateChat.tsx
+++ app/components/KommunicateChat.tsx
@@ -1,39 +1,25 @@
 "use client";
 
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    kommunicate?: any;
-  }
-}
+import Script from 'next/script';
 
 const KommunicateChat = () => {
-  useEffect(() => {
-    // Prevent loading script multiple times
-    if (window.kommunicate) return;
-
-    (function (d, m) {
-      const kommunicateSettings = {
-        appId: "18a554ced79c986e2cbf1382d6b4f76ef",
-        popupWidget: true,
-        automaticChatOpenOnNavigation: true,
-      };
-
-      const s = document.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-      const h = d.getElementsByTagName("head")[0];
-      h.appendChild(s);
-
-      (window as any).kommunicate = m;
-      m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
-  }, []);
-
-  return null;
+  return (
+    <Script
+      strategy="lazyOnload"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(d, m){
+            var kommunicateSettings = 
+                {"appId":"20b5a05884c771f47c6cf7f6688f49096","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+            window.kommunicate = m; m._globals = kommunicateSettings;
+          })(document, window.kommunicate || {});
+        `,
+      }}
+    />
+  );
 };
 
 export default KommunicateChat;
-
