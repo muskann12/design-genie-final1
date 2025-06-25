c6daf1ef254e3a3a14e77ec1388e4c088e6dbe3d
ahmedraza <aaza60232@gmail.com>
2025-06-26 03:28:14 +0500

    changes

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -3,6 +3,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const mongoUri = process.env.MONGODB_URI!;
 
+
 if (!mongoUri) {
   throw new Error("MONGODB_URI is not defined in environment variables");
 }
