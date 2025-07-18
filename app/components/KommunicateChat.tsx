98962008a6431dd584801cb1f6f24d2497604c92
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2025-07-18 17:10:10 +0500

    Update KommunicateChat.tsx

--- app/components/KommunicateChat.tsx
+++ app/components/KommunicateChat.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import Script from 'next/script';
+import Script from "next/script";
 
 const KommunicateChat = () => {
   return (
-    <Script
-      strategy="lazyOnload"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(d, m){
-            var kommunicateSettings = 
-                {"appId":"20b5a05884c771f47c6cf7f6688f49096","popupWidget":true,"automaticChatOpenOnNavigation":true};
-            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-            window.kommunicate = m; m._globals = kommunicateSettings;
-          })(document, window.kommunicate || {});
-        `,
-      }}
-    />
+    <>
+      <Script
+        id="kommunicate-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d, m){
+                var kommunicateSettings = 
+                    {"appId":"33ff3567a177b27e873a16b764ac77f67","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+                s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+                var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+                window.kommunicate = m; m._globals = kommunicateSettings;
+            })(document, window.kommunicate || {});
+          `,
+        }}
+      />
+    </>
   );
 };
 
