ca9ea2208e5732caef6779cadcaf77aa95369dcb
MUHAMMADSHAHRUKH459 <freelacing123@gmail.com>
2025-09-03 06:44:19 +0500

    update shop page, footer, logo, chatbot, and m-product files and add fav icon

--- app/components/KommunicateChat.tsx
+++ app/components/KommunicateChat.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { useEffect } from "react";
 
+// Extend Window type to include kommunicate
+declare global {
+  interface Window {
+    kommunicate: any;
+  }
+}
+
 export default function KommunicateChat() {
   useEffect(() => {
     (function (d, m) {
